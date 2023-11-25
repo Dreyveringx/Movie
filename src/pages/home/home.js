f41b38ddedae7cf37a973d70158d5f9ce9932383
Jair De Jesus Uribe Valencia <xdreyveringx@hotmail.com>
2023-11-24 22:47:13 -0500

    Style Responsive

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,19 +1,29 @@
-import React, { useEffect, useState } from "react"
-import "./home.css"
+import React, { useEffect, useState } from "react";
+import "./home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from "react-router-dom";
 import MovieList from "../../components/movieList/movieList";
 
 const Home = () => {
-
-    const [ popularMovies, setPopularMovies ] = useState([])
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 480);
 
     useEffect(() => {
         fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=es-ES")
         .then(res => res.json())
-        .then(data => setPopularMovies(data.results))
-    }, [])
+        .then(data => setPopularMovies(data.results));
+
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 480);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <>
@@ -27,20 +37,22 @@ const Home = () => {
                 >
                     {
                         popularMovies.map(movie => (
-                            <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
+                            <Link style={{ textDecoration: "none", color: "white" }} to={`/movie/${movie.id}`} key={movie.id}>
                                 <div className="posterImage">
-                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
+                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} alt={movie.original_title} />
                                 </div>
                                 <div className="posterImage__overlay">
-                                    <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
+                                    <div className="posterImage__title">{movie ? movie.original_title : ""}</div>
                                     <div className="posterImage__runtime">
                                         {movie ? movie.release_date : ""}
                                         <span className="posterImage__rating">
-                                            {movie ? movie.vote_average :""}
+                                            {movie ? movie.vote_average : ""}
                                             <i className="fas fa-star" />{" "}
                                         </span>
                                     </div>
-                                    <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+                                    {!isMobile && (
+                                        <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+                                    )}
                                 </div>
                             </Link>
                         ))
@@ -52,4 +64,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default Home;
